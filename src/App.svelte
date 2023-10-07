2e16af58861091e045941a80ec3586c222abb966
faidfadjri <faidfadjri.busdev@gmail.com>
2023-10-08 01:00:49 +0700

    Conditional Statement

--- src/App.svelte
+++ src/App.svelte
@@ -8,9 +8,19 @@
   const onDelete = (person) => {
     people = people.filter((current) => current.id != person.id);
   };
+
+  let num = 28;
 </script>
 
 <main>
+  <!-- Conditional Statement -->
+
+  {#if num > 20}
+    <p>greater than 20</p>
+  {:else if num > 5}
+    <p>greater than 5</p>
+  {/if}
+
   <!-- each refer to map on react & (person.id) refer to keys on react -->
   {#each people as person (person.id)}
     <div>
---
+++ src/courses/Conditional.svelte
@@ -0,0 +1,12 @@
+<script>
+  let num = 28;
+</script>
+
+<main>
+  <!-- Conditional Statement -->
+  {#if num > 20}
+    <p>greater than 20</p>
+  {:else if num > 5}
+    <p>greater than 5</p>
+  {/if}
+</main>
