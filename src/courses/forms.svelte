c2c1d491610572813af40c6d8ae132813d05dca2
faidfadjri <faidfadjri.busdev@gmail.com>
2023-10-08 23:47:31 +0700

    Form

--- src/courses/forms.svelte
+++ src/courses/forms.svelte
@@ -2,15 +2,50 @@
   let firstName;
   let LastName;
 
+  let skills = [];
+
   const onSubmit = () => {
     console.log("FirstName : " + firstName);
     console.log("FirstName : " + LastName);
+    console.table(skills);
   };
 </script>
 
 <form on:submit|preventDefault={onSubmit}>
   <input type="text" placeholder="firstName" bind:value={firstName} />
   <input type="text" placeholder="LastName" bind:value={LastName} />
+
+  <p>Your Skills</p>
+  <ul>
+    <li>
+      <label for="Fighting" class="checkboxes">
+        <input
+          id="Fighting"
+          type="checkbox"
+          bind:group={skills}
+          value="Fighting"
+        />
+        Fighting
+      </label>
+    </li>
+    <li>
+      <label for="Programming" class="checkboxes">
+        <input
+          id="Programming"
+          type="checkbox"
+          bind:group={skills}
+          value="Programming"
+        />
+        Programming
+      </label>
+    </li>
+    <li>
+      <label for="Arts" class="checkboxes">
+        <input id="Arts" type="checkbox" bind:group={skills} value="Arts" />
+        Arts
+      </label>
+    </li>
+  </ul>
   <button>Submit</button>
 </form>
 
@@ -23,5 +58,18 @@
 
   input {
     padding: 10px;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0px;
+    display: flex;
+    flex-direction: column;
+    align-items: self-start;
+    gap: 10px;
+  }
+
+  li {
+    padding: 0px;
   }
 </style>
